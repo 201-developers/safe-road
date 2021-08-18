0b62656af402b3f328341e344d93d3066953391a
dlstodyo <dudgks3316@naver.com>
2021-08-18 22:46:30 +0900

    폴더 구조 변경

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './styles.css';
+import SideBar from './sidebar/sidebar';
+import seoulmap from './seoulmap.jpg'
+import HeaderContainer from './components/Header/HeaderContainer';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div id='App'>
+      <SideBar pageWrapId={'page-wrap'} outerContainerId={'App'} />
+
+      <div id='page-wrap'>
+        <h1>여성 아동 안심거리&지킴이집 길라잡이</h1>
+        <img alt='seoulmap' className='seoulmap' src={seoulmap} />
+        
+        <HeaderContainer />
+      </div>
+      </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
